ad51273b0b337dd9f1482b34197be40f99e9a330
EveDavidLouis <eve.david.louis@gmail.com>
2021-03-02 19:48:39 +0100

    Update pubsub.js

--- docs/scripts/pubsub.js
+++ docs/scripts/pubsub.js
@@ -1,4 +1,5 @@
-var socket, host
+var socketISS, socketCoPilot
+var host
 
 if (window.location.hostname == '0.0.0.0' || window.location.hostname == 'localhost'){
 	host = 'ws://'+ window.location.host
@@ -6,9 +7,8 @@ if (window.location.hostname == '0.0.0.0' || window.location.hostname == 'localh
 	host = 'wss://pymorpheus.herokuapp.com'
 };
 
-function connect() {
+function connectISS() {
 	socketISS = new WebSocket(host + '/iss');
-	
 
 	socketISS.onmessage = function (msg) {
 		
@@ -19,10 +19,38 @@ function connect() {
 
 }
 
+function connectCoPilot() {
+	socketCoPilot = new WebSocket(host + '/copilot',{
+        headers : {
+		'X-Session': create_UUID()	
+	}
+      	});
+
+	socketCoPilot.onmessage = function (msg) {
+		
+		var data = JSON.parse(msg.data);
+		console.log(data);
+		document.querySelector('#CoPilot').innerHTML = JSON.stringify(data, null, 2);	
+	};
+
+}
+
 window.onload = function () {
-	connect();
+	connecISS();
+	connectCoPilot();
 };
 
 window.onbeforeunload = function () {
-	socket.close();
+	socketISS.close();
+	socketCoPilot.close();
+};
+
+function create_UUID(){
+    var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (dt + Math.random()*16)%16 | 0;
+        dt = Math.floor(dt/16);
+        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+    });
+    return uuid;
 };
